e48ceda3a4bc4370bd4c772f52a9fc5d596c60c4
Avinash M Singh <atomeundwolke@gmail.com>
2020-03-14 19:39:32 -0400

    Update app.py

--- Web App/app.py
+++ Web App/app.py
@@ -13,24 +13,39 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(
     className = 'body',
-    style = {'width': '90%',
-    'margin': 0,
+    style = {'width': '100%',
+    'margin': '0',
     'font-family': 'Georgia'},
     children = [
     html.H1(
         className = "app-header",
-        style = {'background-color': '#efefef',
-        'padding': '10px',
-        'text-align': 'center'},
+        style = {
+        'color': '#F0F8FF',
+        'text-align': 'center',
+        'width': '100%',
+        'height': '50px',
+        'position': 'fixed',
+        'top': '0',
+        'background-image': 'url(https://i.imgur.com/D99RLEF.jpg)',
+        'background-size': 'contain'},
         children = [
         html.Div('COVID-19 Outbreak Visualization Tool', className = "app-header--title")
         ]
         ),
     html.Div([
-    dcc.Tabs(id="tabs", value='tab-1', children=[
+    dcc.Tabs(id="tabs", value='tab-1', 
+    	style = {'padding': '30px',
+    	'margin': '0 auto',
+    	'list-style': 'none',
+    	'text-align': 'center',
+    	'width': '100%'
+    	},
+    	children=[
         dcc.Tab(label='3D Nucleocapsid Model', value='tab-1'),
         dcc.Tab(label='Global Tracking Map', value='tab-2'),
-        dcc.Tab(label='Infections in the US', value='tab-3')
+        dcc.Tab(label='Infections in the US', value='tab-3'),
+        dcc.Tab(label='Information on COVID-19', value='tab-4'),
+        dcc.Tab(label='How to Stay Safe', value='tab-5')
     ]),
     html.Div(id='tabs-content')
 ])
@@ -51,6 +66,14 @@ def render_content(tab):
         return html.Div([
             html.H3('Tab content 3')
             ])
+    elif tab == 'tab-4':
+        return html.Div([
+            html.H3('Tab content 4')
+            ])
+    elif tab == 'tab-5':
+        return html.Div([
+            html.H3('Tab content 5')
+            ])
             # html.Li(
             #     children = [
             #     html.Div('3D Nucleocapsid Viewer')
@@ -63,7 +86,7 @@ def render_content(tab):
             #     children = [
             #     html.Div('Infections in US')
             #     ])
-        
+        	#'background-color': '#F0F8FF',
 
 if __name__ == '__main__' :
     app.run_server(debug=True)
