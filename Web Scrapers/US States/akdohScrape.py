0aef87fed5b3497f972402e06a7cec1b32e54f3a
Avinash M Singh <atomeundwolke@gmail.com>
2020-03-29 14:45:30 -0400

    rework

--- Web Scrapers/US States/akdohScrape.py
+++ Web Scrapers/US States/akdohScrape.py
@@ -2,6 +2,7 @@ import bs4
 from urllib.request import urlopen as req
 from bs4 import BeautifulSoup as soup
 
+
 akdoh = 'http://dhss.alaska.gov/dph/Epi/id/Pages/COVID-19/monitoring.aspx'
 
 akClient = req(akdoh)
@@ -9,23 +10,66 @@ akClient = req(akdoh)
 site_parse = soup(akClient.read(), 'lxml')
 akClient.close()
 
-tables = site_parse.find("div", {"class": "grid2"})
+tables = site_parse.find("div", {"class": "grid3"})
 
-regTable = tables.find("table")
+#regTable = tables.find("table")
 
 tags = tables.findAll('tr')
 
 csvfile = "COVID-19_cases_akdoh.csv"
-headers = "Region, Travel Related, Non Travel Related, Total\n"
-
+headers = "Region, Total Cases \n"
+    
 with open(csvfile, 'w', encoding = 'utf-8') as f:
     f.write(headers)
-
-    for tag in tags[1:8]:
-        pull = tag.findAll('p')
-        print("Region = %s, Travel Related = %s, Non-Travel Related = %s, Total = %s" % \
-              (pull[0].text, pull[1].text, pull[2].text, pull[3].text))
-        f.write(pull[0].text + ", " + pull[1].text + ", " + pull[2].text + ", " + pull[3].text + "\n")
+    
+    anch = tags[1].text.split('\n')
+    anR = anch[1].split('\xa0')
+    anR1 = anR[1]
+    anT = anch[6]
+    f.write(anR1 + ", " + anT + "\n")
+    
+    gulf = tags[5].text.split('\n')
+    gulfR = gulf[1].split('\xa0')
+    gulfR1 = gulfR[1]
+    gulfT = gulf[6]
+    f.write(gulfR1 + ", " + gulfT +"\n")
+    
+    intr = tags[10].text.split('\n')
+    intrR = intr[1].split('\xa0')
+    intrR1 = intrR[1]
+    intrT = intr[6]
+    f.write(intrR1 + ", " + intrT + "\n")
+    
+    matsu = tags[13].text.split('\n')
+    matsuR = matsu[1].split('\xa0')
+    matsuR1 = matsuR[1]
+    matsuT = matsu[6]
+    f.write(matsuR1 + ", " + matsuT + "\n")
+    
+    nort = tags[15].text.split('\n')
+    nortR = nort[1].split('\xa0')
+    nortR1 = nortR[1]
+    nortT = nort[6]
+    f.write(nortR1 + ", " + nortT + "\n")
+    
+    se = tags[16].text.split('\n')
+    seR = se[1].split('\xa0')
+    seR1 = seR[1]
+    seT = se[6]
+    f.write(seR1 + ", " + seT + "\n")
+    
+    sw = tags[19].text.split('\n')
+    swR = sw[1].split('\xa0')
+    swR1 = swR[1]
+    swT = sw[6]
+    f.write(swR1 + ", " + swT + "\n")
+    
+    
+#    for tag in tags[1:]:
+#        pull = tag.findAll('p')
+#        print("Region = %s, Total = %s" % \
+#              (pull[0].text, pull[5].text))
+#        f.write(pull[0].text + ", " + pull[5].text + "\n")
 
 f.close()
-    
+
--- Web Scrapers/US States/aldohScrape.py
+++ Web Scrapers/US States/aldohScrape.py
@@ -1,29 +1,30 @@
 import bs4
+import json
 from urllib.request import urlopen as req
 from bs4 import BeautifulSoup as soup
 
-aldoh = 'http://www.alabamapublichealth.gov/infectiousdiseases/2019-coronavirus.html'
+aldoh = 'https://services7.arcgis.com/4RQmZZ0yaZkGR1zy/arcgis/rest/services/COV19_Public_Dashboard_ReadOnly/FeatureServer/0/query?where=1%3D1&outFields=CNTYNAME%2CCNTYFIPS%2CCONFIRMED%2CDIED&returnGeometry=false&f=pjson'
 
-alClient = req(aldoh)
+alClient = req(aldoh).read().decode('utf-8')
 
-site_parse = soup(alClient.read(), 'lxml')
-alClient.close()
+rJS = json.loads(response)
 
-tables = site_parse.find("div", {"class": "row mainContent"})
-
-tags = tables.findAll('tr')
+attr = rJS.get('features')
 
 csvfile = "COVID-19_cases_aldoh.csv"
-headers = "County, Positive Cases \n"
+headers = "County, Positive Cases, Deaths \n"
 
 file = open(csvfile, "w")
 file.write(headers)
 
+for a in attr:
+    file.write(a.get('attributes').get('CNTYNAME') + ", " + str(a.get('attributes').get('CONFIRMED')) + ", " + str(a.get('attributes').get('DIED')) + "\n")
 
-for tag in tags[2:23]:
-    pull = tag.findAll('p')
-    print("County = %s, Positive Cases = %s" % (pull[0].text, pull[1].text))
-    
-    file.write(pull[0].text + ", " + pull[1].text + "\n")
 
+#for tag in tags[2:23]:
+#    pull = tag.findAll('p')
+#    print("County = %s, Positive Cases = %s" % (pull[0].text, pull[1].text))
+#    
+#    file.write(pull[0].text + ", " + pull[1].text + "\n")
+#
 file.close()
