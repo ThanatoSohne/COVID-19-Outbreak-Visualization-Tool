114c2dd034afc2c6d50236a7cbc42e2a29da41ce
MaryRamos <47065511+MaryRamos@users.noreply.github.com>
2020-05-03 20:42:15 -0400

    PredictionModel.py
    Revised PredictionModel.py

--- PredictionModel.py
+++ PredictionModel.py
@@ -17,9 +17,33 @@ import math
 import datetime
 
 
+# df = pd.read_csv('https://raw.githubusercontent.com/datasets/covid-19/master/data/time-series-19-covid-combined.csv')
+
+# newAgain = pd.DataFrame(columns= ['Date','Country/Region','Confirmed', 'Recovered','Deaths'])
+
+# rose = []
+# test = list(df['Country/Region'].unique())
+
+# for i in test:
+    # niles = df.groupby('Country/Region').get_group(i)
+    # rose.append(niles)
+# for r in rose:
+    # hold=[]
+    # timeD = r['Date'].tolist()
+    # for t in timeD:
+        # delt = datetime.datetime.strptime(t, "%Y-%m-%d").strftime("%m%d%Y")
+        # hold.append(delt)
+    # r['date'] = hold
+    # newAgain = pd.concat([newAgain, r])
+
+# newAgain = newAgain.drop(columns = 'Date').astype({'date':'float64'})
+# newAgain = newAgain.reindex(columns = ['date','Country/Region','Confirmed', 'Recovered','Deaths'])
+
+
 df = pd.read_csv('https://raw.githubusercontent.com/datasets/covid-19/master/data/time-series-19-covid-combined.csv')
 
-newAgain = pd.DataFrame(columns= ['Date','Country/Region','Confirmed', 'Recovered','Deaths'])
+col_name = ['Date', 'Country/Region', 'Province/State', 'Lat', 'Long', 'Confirmed', 'Recovered', 'Deaths', 'date']
+newAgain = pd.DataFrame(columns = col_name)
 
 rose = []
 test = list(df['Country/Region'].unique())
@@ -37,14 +61,18 @@ for r in rose:
     newAgain = pd.concat([newAgain, r])
 
 newAgain = newAgain.drop(columns = 'Date').astype({'date':'float64'})
-newAgain = newAgain.reindex(columns = ['date','Country/Region','Confirmed', 'Recovered','Deaths'])
+
+newAgain = newAgain.reindex(columns = ['date', 'Country/Region', 'Province/State', 'Lat', 'Long', 'Confirmed', 'Recovered', 'Deaths'])
+print(newAgain)
+
+
 
 
 filtered_data = df[newAgain["Country/Region"] =='US']
 print(filtered_data)
 
-x = newAgain.date
-y1 = newAgain.Confirmed
+x = filtered_data.Date.values.reshape(-1,1)
+y1 = filtered_data.Confirmed.values.reshape(-1,1)
 
 
 
@@ -69,3 +97,5 @@ fdf['Predicted'] = np.round(test_prediction, 1)
 fdf['Prediction_Error_Confirmed'] = fdf['Confirmed'] - fdf['Predicted']  
 fdf
 print(fdf)
+
+filtered_data.plot(x = 'Date', y = 'Confirmed', style = 'o')
